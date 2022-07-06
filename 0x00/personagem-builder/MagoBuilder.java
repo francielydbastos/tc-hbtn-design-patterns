5f938f5aa6c211ab41d6902cd3f9ad82df57c9f0
francielydbastos <francielydbastos@gmail.com>
2022-07-06 15:21:15 -0300

    Padrões de Projeto 0 Task 1

---
+++ 0x00/personagem-builder/MagoBuilder.java
@@ -0,0 +1,50 @@
+package builder;
+
+public class MagoBuilder implements Builder {
+    private String nome;
+    private TipoPersonagem tipo;
+    private int inteligencia;
+    private int forca;
+    private int vigor;
+    private int resistencia;
+    private int destreza;
+
+    @Override
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void setTipo(TipoPersonagem tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+
+    @Override
+    public void setForca(int forca) {
+        this.forca = forca;
+    }
+
+    @Override
+    public void setVigor(int vigor) {
+        this.vigor = vigor;
+    }
+
+    @Override
+    public void setResistencia(int destreza) {
+        this.destreza = destreza;
+    }
+
+    @Override
+    public void setDestreza(int destreza) {
+        this.destreza = destreza;
+    }
+
+    public Mago build() {
+        return new Mago(this.nome, this.inteligencia, this.forca, this.vigor, this.resistencia, this.destreza);
+    }
+}
---
+++ 0x00/personagem-builder/TipoPersonagem.java
@@ -0,0 +1,7 @@
+package builder;
+
+public enum TipoPersonagem {
+    GUERREIRO,
+    MAGO,
+    LADRAO
+}
