8a39dd3534e6d77b82702925d2b8768c703264bf
francielydbastos <francielydbastos@gmail.com>
2022-07-06 15:35:44 -0300

    Padrões de Projeto 0 Task 1

--- 0x00/personagem-builder/MagoBuilder.java
+++ 0x00/personagem-builder/MagoBuilder.java
@@ -33,8 +33,8 @@ public class MagoBuilder implements Builder {
     }
 
     @Override
-    public void setResistencia(int destreza) {
-        this.destreza = destreza;
+    public void setResistencia(int resistencia) {
+        this.resistencia = resistencia;
     }
 
     @Override
