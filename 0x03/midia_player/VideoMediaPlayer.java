e8007c821cd87667909b9d4d9fc3bdd83ff3c706
francielydbastos <francielydbastos@gmail.com>
2022-07-11 09:26:20 -0300

    Padrões de Projeto 3 Task 1

--- 0x03/midia_player/VideoMediaPlayer.java
+++ 0x03/midia_player/VideoMediaPlayer.java
@@ -2,12 +2,12 @@ public class VideoMediaPlayer implements AdvancedMediaPlayer {
 
     @Override
     public void reproduzirVlc(String nomeVlc) {
-        System.out.println("Reproduzindo VLC: " + nomeVlc);
+        System.out.print("Reproduzindo VLC: " + nomeVlc);
     }
 
     @Override
     public void reproduzirMp4(String nomeMP4) {
-        System.out.println("Reproduzindo MP4: " + nomeMP4);
+        System.out.print("Reproduzindo MP4: " + nomeMP4);
     }
 
 }
